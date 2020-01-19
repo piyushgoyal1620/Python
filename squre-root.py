f02e0ccddfc96b50f13f0cd1810be217445cb5c3
Piyush Goyal <52343801+piyushgoyal1620@users.noreply.github.com>
2020-01-19 22:17:24 +0530

    Create squre-root.py

---
+++ squre-root.py
@@ -0,0 +1,13 @@
+n=int(input("enter a number: "))
+for i in range(n):
+  if i*i==n:
+    print(i)
+    break
+  if i*i>n:
+    j=i-1
+    while j<=i:
+      if j*j>n:
+        print(j)
+        break
+      j=j+0.01
+    break
